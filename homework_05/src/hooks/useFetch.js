b364b669bd0d1e7c7a67d21d052b297367e507e7
Sofiia Ruban <sofiia.ruban@intellias.com>
2026-06-28 11:27:53 +0300

    feat: add useFetch hook

---
+++ homework_05/src/hooks/useFetch.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+export function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, isLoading, error };
+}
